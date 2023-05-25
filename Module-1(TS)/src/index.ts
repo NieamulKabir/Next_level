56ee243ba0f043cd369c4040adaa9ccd7b92af35
NieamulKabir <kabir000079@gmail.com>
2023-05-25 10:31:33 +0600

    array

--- Module-1(TS)/src/index.ts
+++ Module-1(TS)/src/index.ts
@@ -1,5 +1,12 @@
-const course:string="Next Level  Development";
+let course: string = "Next Level  Development";
+course = "Next Level Web Development";
 
-console.log(course);
-console.log('hello');
-console.log('hello');
+let rollNumber: number = 1924;
+rollNumber = 181 - 15 - 1924;
+
+let isAdmin: boolean = true;
+
+//Primitive type: string, number,boolean,null , undefined
+
+
+ 
