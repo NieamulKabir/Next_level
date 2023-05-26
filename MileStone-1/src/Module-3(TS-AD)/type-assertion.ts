0e0f1714a432ab2515a1b17cf107362048bb8caf
NieamulKabir <kabir000079@gmail.com>
2023-05-26 23:51:48 +0600

    update

--- MileStone-1/src/Module-3(TS-AD)/type-assertion.ts
+++ MileStone-1/src/Module-3(TS-AD)/type-assertion.ts
@@ -1,3 +1,15 @@
-let emni:any;
+let emni: any;
 
-(emni as string)="Next Level Web Development";
+(emni as string) = "Next Level Web Development";
+
+function kgToGram(param: string | number): string | number | undefined {
+    if (typeof param === "string") {
+        const value = parseFloat(param) * 1000;
+        return `The Converted result is: ${value} gram`;
+    } else if (typeof param === "number") {
+        const value = param * 1000;
+        return `The Converted result is: ${value} gram`;
+    }
+}
+const resultToBeNumber = kgToGram(1000) as number;
+const resultToBeString = kgToGram(1000) as string;
