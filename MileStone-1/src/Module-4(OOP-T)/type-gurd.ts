40e9d19af21ff0a006a84d1b29d05ef03c6a659c
NieamulKabir <kabir000079@gmail.com>
2023-06-02 23:03:16 +0600

    type guard

--- MileStone-1/src/Module-4(OOP-T)/type-gurd.ts
+++ MileStone-1/src/Module-4(OOP-T)/type-gurd.ts
@@ -36,3 +36,59 @@ const normalUser1: NormalUserType = {
 const adminUser1: AdminUserType = { name: "Mr. Gallu", role: "admin" };
 
 console.log(getUser(normalUser1));
+console.log(getUser(adminUser1));
+
+//instance of guard
+
+class Animal1 {
+    name: string;
+    species: string;
+
+    constructor(name: string, species: string) {
+        this.name = name
+        this.species = species
+    }
+    makeSound() {
+        console.log('I am Making Sound');
+    }
+};
+
+class Dog extends Animal1 {
+    constructor(name: string, species: string) {
+        super(name, species)
+    }
+    makeBark() {
+        console.log("I am Barking");
+    }
+};
+
+class Cat extends Animal1 {
+    constructor(name: string, species: string) {
+        super(name, species)
+    }
+    makeMew() {
+        console.log("Mew Mew");
+    }
+};
+
+function isDog(animal: Animal1): animal is Dog {
+    return animal instanceof Dog;
+};
+function isCat(animal: Animal1): animal is Cat {
+    return animal instanceof Cat;
+};
+
+function getAnimal(animal: Animal1) {
+    if (isDog(animal)) {
+        animal.makeBark()
+    }
+    else if (isCat(animal)) {
+        animal.makeMew();
+    }
+    else {
+        animal.makeSound();
+    }
+}
+
+const animal10 = new Dog("German Bhai", "Dog");
+const animal11 = new Cat("Cat", "Cat");
