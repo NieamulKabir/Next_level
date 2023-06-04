f3e732dd4575a6cf6682563cd03026ba15a94eca
NieamulKabir <kabir000079@gmail.com>
2023-06-04 16:46:48 +0600

    getter-setter

--- MileStone-1/src/Module-4(OOP-T)/access-modifiers.ts
+++ MileStone-1/src/Module-4(OOP-T)/access-modifiers.ts
@@ -1,25 +1,25 @@
-class BankAccount {
-    public id: number;
-    public name: string;
-    private _balance: number;
+// class BankAccount {
+//     public id: number;
+//     public name: string;
+//     private _balance: number;
 
-    constructor(id: number, name: string, balance: number) {
-        this.id = id;
-        this.name = name;
-        this._balance = balance;
-    };
-    getBAlance() {
-        console.log(`My current balance is ${this._balance}`);
-    };
-    addDeposit(amount: number) {
-        this._balance = this._balance + amount;
-    };
-};
+//     constructor(id: number, name: string, balance: number) {
+//         this.id = id;
+//         this.name = name;
+//         this._balance = balance;
+//     };
+//     getBAlance() {
+//         console.log(`My current balance is ${this._balance}`);
+//     };
+//     addDeposit(amount: number) {
+//         this._balance = this._balance + amount;
+//     };
+// };
 
-class StudentAccount extends BankAccount{
+// class StudentAccount extends BankAccount{
     
-}
+// }
 
-const myAccount = new BankAccount(1924, "Kabir", 20);
-// myAccount.balance = 0;
-console.log(myAccount);
+// const myAccount = new BankAccount(1924, "Kabir", 20);
+// // myAccount.balance = 0;
+// console.log(myAccount);
---
+++ MileStone-1/src/Module-4(OOP-T)/getter-setter.ts
@@ -0,0 +1,35 @@
+class BankAccount {
+    public id: number;
+    public name: string;
+    private _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+    };
+    //getter
+    get balance(): number {
+        return this._balance;
+    }
+    //setter
+    set Deposit(amount: number) {
+         this._balance = this._balance + amount;
+    }
+    getBAlance() {
+        console.log(`My current balance is ${this._balance}`);
+    };
+    addDeposit(amount: number) {
+        return this._balance = this._balance + amount;
+    };
+};
+
+const myAccount = new BankAccount(1924, "Kabir", 20);
+// myAccount.balance = 0;
+console.log(myAccount);
+console.log(myAccount.addDeposit(20));
+myAccount.getBAlance();
+
+console.log(myAccount.balance);
+myAccount.Deposit = 20;
+console.log(myAccount.balance);
