a520f67e4e7a83042eb894b25d28ec075901d91c
NieamulKabir <kabir000079@gmail.com>
2023-06-06 21:28:23 +0600

    update

--- MileStone-1/src/Module-2/array.ts
+++ MileStone-1/src/Module-2/array.ts
@@ -5,3 +5,5 @@ const all = ["Kabir", "Afra", true, 2];
 all.push(10)//we can push any value in all variable
 
 const user: [number, string,boolean] = [1924, "Nieamul",true] ;//tuple
+
+const user2:[number,boolean]=[11,true]
